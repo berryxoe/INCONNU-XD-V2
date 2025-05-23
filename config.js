c5e0cbd804a77dc949017392d62ea3507376b2b2
berryxoe <tudawens@gmail.com>
2025-05-22 22:42:24 -0400

    config.js
    Signed-off-by: berryxoe <tudawens@gmail.com>

--- config.js
+++ config.js
@@ -3,33 +3,38 @@ const fs = require('fs-extra');
 const { Sequelize } = require('sequelize');
 if (fs.existsSync('config.env'))
     require('dotenv').config({ path: __dirname + '/config.env' });
+
 const path = require("path");
 const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
-module.exports = { session: process.env.SESSION_ID || '',
+
+module.exports = {
+    session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "INCONNU BOY",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || " 554488138425",              
+    OWNER_NAME: process.env.OWNER_NAME || "DAWENS V7",
+    NUMERO_OWNER: process.env.NUMERO_OWNER || "50942241547",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
-    BOT : process.env.BOT_NAME || 'INCONNU-XD-V2',
-    URL : process.env.BOT_MENU_LINKS || 'https://files.catbox.moe/959dyk.jpg',
+    BOT: process.env.BOT_NAME || 'DAWENS-XD-V2',
+    URL: process.env.BOT_MENU_LINKS || 'https://files.catbox.moe/959dyk.jpg',
     MODE: process.env.PUBLIC_MODE || "yes",
     PM_PERMIT: process.env.PM_PERMIT || 'yes',
-    HEROKU_APP_NAME : process.env.HEROKU_APP_NAME,
-    HEROKU_APY_KEY : process.env.HEROKU_APY_KEY ,
-    WARN_COUNT : process.env.WARN_COUNT || '3' ,
-    ETAT : process.env.PRESENCE || '',
-    CHATBOT : process.env.CHATBOT || 'no',
-    DP : process.env.STARTING_BOT_MESSAGE || "yes",
-    ADM : process.env.ANTI_DELETE_MESSAGE || 'no',
+    HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
+    HEROKU_APY_KEY: process.env.HEROKU_APY_KEY,
+    WARN_COUNT: process.env.WARN_COUNT || '3',
+    ETAT: process.env.PRESENCE || '',
+    CHATBOT: process.env.CHATBOT || 'no',
+    DP: process.env.STARTING_BOT_MESSAGE || "yes",
+    ADM: process.env.ANTI_DELETE_MESSAGE || 'no',
+    ANTI_DAVE_MASISI: process.env.ANTI_DAVE_MASISI || 'true',
     DATABASE_URL,
     DATABASE: DATABASE_URL === databasePath
-        ? "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway" : "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway",
-   
+        ? "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway"
+        : "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway",
 };
+
 let fichier = require.resolve(__filename);
 fs.watchFile(fichier, () => {
     fs.unwatchFile(fichier);
@@ -37,4 +42,3 @@ fs.watchFile(fichier, () => {
     delete require.cache[fichier];
     require(fichier);
 });
-                               
